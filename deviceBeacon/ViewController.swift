920751ef57767b4de74cb2e70c82240e60dbcef8
Mauricio Lorenzetti <mauricio@LEGEND-MBP.local>
2017-10-27 21:01:10 -0200

    device beacon zero

--- deviceBeacon/ViewController.swift
+++ deviceBeacon/ViewController.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
 class ViewController: UIViewController {
 
+    let myUUID = "78908CFE-28EE-4443-85B7-3BD0A6FF0E7E"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        advertiseDevice(region: createBeaconRegion()!)
+    }
+    
+    func createBeaconRegion() -> CLBeaconRegion? {
+        let proximityUUID = UUID(uuidString: myUUID)
+        let major : CLBeaconMajorValue = 100
+        let minor : CLBeaconMinorValue = 1
+        let beaconID = "bubble.Beacon"
+        
+        return CLBeaconRegion(proximityUUID: proximityUUID!, major: major, minor: minor, identifier: beaconID)
+    }
+    
+    func advertiseDevice(region : CLBeaconRegion) {
+        let peripheral = CBPeripheralManager(delegate: self, queue: nil)
+        let peripheralData = region.peripheralData(withMeasuredPower: nil)
+        
+        peripheral.startAdvertising(((peripheralData as NSDictionary) as! [String : Any]))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+}
 
+extension ViewController :CBPeripheralManagerDelegate {
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        print("updated")
+    }
 }
 
